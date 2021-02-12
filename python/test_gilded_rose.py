b52263d641f58d9854922914279ebaf1de8c56d4
michael delbeke <michael.delbeke@hotmail.com>
2021-02-12 09:27:53 +0100

    added quality degration test

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -3,12 +3,14 @@ import unittest
 
 from gilded_rose import Item, GildedRose
 
+
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_quality_degretation(self):
+        items = [Item("foo", sell_in=5, quality=20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual(19, items[0].quality)
+
 
 if __name__ == '__main__':
     unittest.main()
