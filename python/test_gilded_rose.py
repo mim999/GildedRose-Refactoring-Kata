e96096fffd26260283270b179295a2d6681f9812
michael delbeke <michael.delbeke@hotmail.com>
2021-02-12 10:38:22 +0100

    added comentary

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,42 +5,49 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
+    # tests if quality decreases with time on normal item
     def test_quality_degretation(self):
         items = [Item("foo", sell_in=5, quality=20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(19, items[0].quality)
 
+    # tests if time decreases
     def test_time_degretation(self):
         items = [Item("foo", sell_in=5, quality=20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(4, items[0].sell_in)
 
+    # tests if quality decrease double after sellin passed
     def test_sellInPassed_quality_degretation(self):
         items = [Item("foo", sell_in=0, quality=20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(18, items[0].quality)
 
+    # tests if quality can't be negative
     def test_quality_negative(self):
         items = [Item("foo", sell_in=5, quality=0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertFalse(0 > items[0].quality)
 
+    # tests if quality increased of brie with time
     def test_brie_quality_increase(self):
         items = [Item("Aged Brie", sell_in=2, quality=0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(1, items[0].quality)
 
+    # test if quality maxes out at 50
     def test_quality_max(self):
         items = [Item("Aged Brie", sell_in=2, quality=50)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertTrue(items[0].quality <= 50)
 
+    # tests if sulfuras values stay the same
     def test_sulfuras(self):
         items = [Item("Sulfuras, Hand of Ragnaros", sell_in=0, quality=80)]
         gilded_rose = GildedRose(items)
@@ -48,6 +55,7 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(80, items[0].quality)
         self.assertEqual(0, items[0].sell_in)
 
+    # test backstage increase by one
     def test_backstage_increase_by_one(self):
         items = [Item(name="Backstage passes to a TAFKAL80ETC concert",
                       sell_in=15, quality=20)
@@ -56,6 +64,7 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(21, items[0].quality)
 
+    # test backstage increase by two
     def test_backstage_increase_by_two(self):
         items = [
             Item(name="Backstage passes to a TAFKAL80ETC concert",
@@ -65,6 +74,7 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(22, items[0].quality)
 
+    # test backstage increase by three
     def test_backstage_increase_by_three(self):
         items = [
             Item(name="Backstage passes to a TAFKAL80ETC concert",
@@ -74,6 +84,7 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(23, items[0].quality)
 
+    # test backstage quality to zero after sellin
     def test_backstage_quality_zero(self):
         items = [
             Item(name="Backstage passes to a TAFKAL80ETC concert",
@@ -83,6 +94,7 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(0, items[0].quality)
 
+    # test conjured loses quality twice as fast
     def test_conjured(self):
         items = [Item(name="Conjured Mana Cake", sell_in=3, quality=6)]
         gilded_rose = GildedRose(items)
