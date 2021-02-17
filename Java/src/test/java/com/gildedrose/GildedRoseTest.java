8d6dc917407281f195701f79a6c9fcdbf287488c
michael delbeke <michael.delbeke@hotmail.com>
2021-02-17 20:44:13 +0100

    converted python unit tests to java

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,15 +3,126 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseTest {
 
+    // tests if quality decreases with time on normal item
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void test_quality_degretation() {
+        Item[] items = new Item[] { new Item("foo", 5, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(19, app.items[0].quality);
+    }
+
+    // tests if time decreases
+    @Test
+    void test_time_degretation() {
+        Item[] items = new Item[] { new Item("foo", 5, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(4, app.items[0].sellIn);
+    }
+
+    // tests if quality decrease double after sellin passed
+    @Test
+    void test_sellInPassed_quality_degretation() {
+        Item[] items = new Item[] { new Item("foo", 0, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(18, app.items[0].quality);
+    }
+
+    // tests if quality can't be negative
+    @Test
+    void test_quality_negative() {
+        Item[] items = new Item[] { new Item("foo", 5, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertFalse(0 > app.items[0].quality);
+    }
+
+    // tests if quality increased of brie with time
+    @Test
+    void test_brie_quality_increase() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(1, app.items[0].quality);
+    }
+
+    // test if quality maxes out at 50
+    @Test
+    void test_quality_max() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertTrue(app.items[0].quality <= 50);
+    }
+
+    // tests if sulfuras sellin stay the same
+    @Test
+    void test_sulfuras_sellin() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].sellIn);
+    }
+
+    // tests if sulfuras quality stay the same
+    @Test
+    void test_sulfuras_quality() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(80, app.items[0].quality);
+    }
+
+    // test backstage increase by one
+    @Test
+    void test_backstage_increase_by_one() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(21, app.items[0].quality);
+    }
+
+    // test backstage increase by two
+    @Test
+    void test_backstage_increase_by_two() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(22, app.items[0].quality);
+    }
+
+    // test backstage increase by three
+    @Test
+    void test_backstage_increase_by_three() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(23, app.items[0].quality);
+    }
+
+    // test backstage quality to zero after sellin
+    @Test
+    void test_backstage_quality_zero() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+
+    // test conjured loses quality twice as fast
+    @Test
+    void test_conjured() {
+        Item[] items = new Item[] { new Item("Conjured Mana Cake", 3, 6) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(4, app.items[0].quality);
     }
 
 }
