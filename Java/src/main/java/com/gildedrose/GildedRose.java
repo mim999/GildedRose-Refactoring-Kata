a524c716225b83614b03d54ca7c75b02cddc1c1e
michael delbeke <michael.delbeke@hotmail.com>
2021-02-17 21:02:55 +0100

    extract sellinPassed method

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -25,22 +25,26 @@ class GildedRose {
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+                sellinPassed(i);
+            }
+        }
+    }
+
+    private void sellinPassed(int i) {
+        if (!items[i].name.equals("Aged Brie")) {
+                if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].quality > 0) {
+                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            items[i].quality = items[i].quality - 1;
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    items[i].quality = items[i].quality - items[i].quality;
                 }
-            }
+            } else {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                }
         }
     }
 
