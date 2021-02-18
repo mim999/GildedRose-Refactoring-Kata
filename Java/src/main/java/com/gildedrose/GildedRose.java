26913046faca6f8ddc2910c30143ae9f1458f1a8
michael delbeke <michael.delbeke@hotmail.com>
2021-02-18 09:10:36 +0100

    changes for loop to foreach

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,66 +13,70 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(AGED_BRIE_NAME)
-                    && !items[i].name.equals(BACKSTAGE_PASS_NAME)) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(SULFURAS_NAME)) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                checkBrie(i);
-            }
-
-            if (!items[i].name.equals(SULFURAS_NAME)) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                sellinPassed(i);
-            }
+        for (Item item: items) {
+            UpdateSingleItem(item);
         }
     }
 
-    private void sellinPassed(int i) {
-        if (!items[i].name.equals(AGED_BRIE_NAME)) {
-                if (!items[i].name.equals(BACKSTAGE_PASS_NAME)) {
-                    if (items[i].quality > 0) {
-                        if (!items[i].name.equals(SULFURAS_NAME)) {
-                            items[i].quality = items[i].quality - 1;
+    private void UpdateSingleItem(Item item) {
+        if (!item.name.equals(AGED_BRIE_NAME)
+                && !item.name.equals(BACKSTAGE_PASS_NAME)) {
+            if (item.quality > 0) {
+                if (!item.name.equals(SULFURAS_NAME)) {
+                    item.quality = item.quality - 1;
+                }
+            }
+        } else {
+            checkBrie(item);
+        }
+
+        if (!item.name.equals(SULFURAS_NAME)) {
+            item.sellIn = item.sellIn - 1;
+        }
+
+        if (item.sellIn < 0) {
+            sellinPassed(item);
+        }
+    }
+
+    private void sellinPassed(Item item) {
+        if (!item.name.equals(AGED_BRIE_NAME)) {
+                if (!item.name.equals(BACKSTAGE_PASS_NAME)) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals(SULFURAS_NAME)) {
+                            item.quality = item.quality - 1;
                         }
                     }
                 } else {
-                    items[i].quality = items[i].quality - items[i].quality;
+                    item.quality = item.quality - item.quality;
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
         }
     }
 
-    private void checkBrie(int i) {
-        if (items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1;
+    private void checkBrie(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
 
-            if (items[i].name.equals(BACKSTAGE_PASS_NAME)) {
-                checkBackstage(i);
+            if (item.name.equals(BACKSTAGE_PASS_NAME)) {
+                checkBackstage(item);
             }
         }
     }
 
-    private void checkBackstage(int i) {
-        if (items[i].sellIn < 11) {
-            if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
+    private void checkBackstage(Item item) {
+        if (item.sellIn < 11) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
             }
         }
 
-        if (items[i].sellIn < 6) {
-            if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
+        if (item.sellIn < 6) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
             }
         }
     }
