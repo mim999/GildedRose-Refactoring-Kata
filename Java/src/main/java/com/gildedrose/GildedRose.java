9618c5ee293de7e7b695ecfd86e1e66e01d35b67
michael delbeke <michael.delbeke@hotmail.com>
2021-02-18 12:40:57 +0100

    added comments

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -15,12 +15,14 @@ class GildedRose {
         this.items = items;
     }
 
+    // runs through all items
     public void updateQuality() {
         for (Item item: items) {
             UpdateSingleItem(item);
         }
     }
 
+    // Updates sellin and then quality
     private void UpdateSingleItem(Item item) {
         updateSellin(item);
         if (item.sellIn < 0) {
@@ -31,16 +33,20 @@ class GildedRose {
         }
     }
 
+    // sellin update
     private void updateSellin(Item item) {
         if (!item.name.equals(SULFURAS_NAME)) {
             item.sellIn = item.sellIn - 1;
         }
     }
 
+    // quality update for non expired
     private void updateQuality(Item item) {
+        // brie increases in quality
         if(item.name.equals(AGED_BRIE_NAME)){
             increaseQuality(item, 1);
         }
+        // backstage increase quality in three stages
         else if(item.name.equals(BACKSTAGE_PASS_NAME)) {
             if (item.sellIn <= BACKSTAGE_PASS_TRIPLE_SELLIN) {
                 increaseQuality(item,3);
@@ -52,33 +58,41 @@ class GildedRose {
                 increaseQuality(item, 1);
             }
         }
+        // conjured decreases twice the speed
         else if(item.name.equals(CONJURED_NAME)){
             decreaseQuality(item,2);
         }
+        // all else decreases by 1
         else if(!item.name.equals(SULFURAS_NAME)){
             decreaseQuality(item,1);
         }
 
     }
 
+    // updates quality of items with sellin < 0
     private void updateQualityExpired(Item item) {
+        // brie increases
         if (item.name.equals(AGED_BRIE_NAME)) {
             increaseQuality(item, 1);
         }
+        // backstage loses value
         else if (item.name.equals(BACKSTAGE_PASS_NAME)) {
             item.quality = 0;
         }
+        // all else loses twice as fast
         else if (!item.name.equals(SULFURAS_NAME)) {
             decreaseQuality(item, 2);
         }
     }
 
+    // Increase items quality by value, can't be over max
     private void increaseQuality(Item item, int value) {
         if (item.quality < MAX_QUALITY) {
             item.quality = item.quality + value;
         }
     }
 
+    // Decrease items quality by value, can't be below zero
     private void decreaseQuality(Item item, int value) {
         if (item.quality > 0) {
             item.quality = item.quality - value;
