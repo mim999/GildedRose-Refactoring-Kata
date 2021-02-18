7b3eb6a3e1acef60e3a2fb39f7a3e3b962347a24
michael delbeke <michael.delbeke@hotmail.com>
2021-02-18 08:39:40 +0100

    changed item names to consts

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,6 +2,11 @@ package com.gildedrose;
 
 class GildedRose {
     Item[] items;
+    public static final String BACKSTAGE_PASS_NAME = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS_NAME = "Sulfuras, Hand of Ragnaros";
+    public static final String AGED_BRIE_NAME = "Aged Brie";
+    public static final String CONJURED_NAME = "Conjured Mana Cake";
+
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -9,10 +14,10 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!items[i].name.equals(AGED_BRIE_NAME)
+                    && !items[i].name.equals(BACKSTAGE_PASS_NAME)) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!items[i].name.equals(SULFURAS_NAME)) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
@@ -20,7 +25,7 @@ class GildedRose {
                 checkBrie(i);
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].name.equals(SULFURAS_NAME)) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
@@ -31,10 +36,10 @@ class GildedRose {
     }
 
     private void sellinPassed(int i) {
-        if (!items[i].name.equals("Aged Brie")) {
-                if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if (!items[i].name.equals(AGED_BRIE_NAME)) {
+                if (!items[i].name.equals(BACKSTAGE_PASS_NAME)) {
                     if (items[i].quality > 0) {
-                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                        if (!items[i].name.equals(SULFURAS_NAME)) {
                             items[i].quality = items[i].quality - 1;
                         }
                     }
@@ -52,7 +57,7 @@ class GildedRose {
         if (items[i].quality < 50) {
             items[i].quality = items[i].quality + 1;
 
-            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (items[i].name.equals(BACKSTAGE_PASS_NAME)) {
                 checkBackstage(i);
             }
         }
